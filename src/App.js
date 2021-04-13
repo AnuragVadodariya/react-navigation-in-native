d577ae7136a5ec9a11524e9d1e5faeab1babcf4d
Anurag Vadodariya <67444494+AnuragVadodariya@users.noreply.github.com>
2021-04-13 13:32:13 +0530

    add styles in react-navigation

--- src/App.js
+++ src/App.js
@@ -12,10 +12,51 @@ const App = () => {
         <Stack.Screen
           name="Home"
           component={HomeScreen}
-          options={{ title: "Welcome" }}
+          options={{
+            title: "My home",
+            headerStyle: {
+              backgroundColor: "#f4511e"
+            },
+            headerTintColor: "#000",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              color: "white",
+              textAlign: "center"
+            }
+          }}
         />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-        <Stack.Screen name="Contact" component={Contact} />
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{
+            title: "Profile",
+            headerStyle: {
+              backgroundColor: "#f4511e"
+            },
+            headerTintColor: "#000",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              color: "white",
+              textAlign: "center"
+            }
+          }}
+        />
+        <Stack.Screen
+          name="Contact"
+          component={Contact}
+          options={{
+            title: "Contact",
+            headerStyle: {
+              backgroundColor: "#f4511e"
+            },
+            headerTintColor: "#000",
+            headerTitleStyle: {
+              fontWeight: "bold",
+              color: "white",
+              textAlign: "center"
+            }
+          }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
