de44890f42da16e73a7af553dc541366c4041dc5
Anurag Vadodariya <67444494+AnuragVadodariya@users.noreply.github.com>
2021-04-13 14:53:50 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -8,53 +8,38 @@ const Stack = createStackNavigator();
 const App = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#f4511e"
+          },
+          headerTintColor: "#000",
+          headerTitleStyle: {
+            fontWeight: "bold",
+            color: "white",
+            textAlign: "center"
+          }
+        }}
+      >
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{
-            title: "My home",
-            headerStyle: {
-              backgroundColor: "#f4511e"
-            },
-            headerTintColor: "#000",
-            headerTitleStyle: {
-              fontWeight: "bold",
-              color: "white",
-              textAlign: "center"
-            }
+            title: "My home"
           }}
         />
         <Stack.Screen
           name="Profile"
           component={ProfileScreen}
           options={{
-            title: "Profile",
-            headerStyle: {
-              backgroundColor: "#f4511e"
-            },
-            headerTintColor: "#000",
-            headerTitleStyle: {
-              fontWeight: "bold",
-              color: "white",
-              textAlign: "center"
-            }
+            title: "Profile"
           }}
         />
         <Stack.Screen
           name="Contact"
           component={Contact}
           options={{
-            title: "Contact",
-            headerStyle: {
-              backgroundColor: "#f4511e"
-            },
-            headerTintColor: "#000",
-            headerTitleStyle: {
-              fontWeight: "bold",
-              color: "white",
-              textAlign: "center"
-            }
+            title: "Contact"
           }}
         />
       </Stack.Navigator>
